19b8417413e28bba66c372cc077d483844f9e50d
Michael Lombardi <mlombardi@microsoft.com>
2023-02-08 14:51:18 -0600

    (GH-42) Add packaging tasks for Vale
    This change:
    
    - Adds a PowerShell build task for packaging Vale style packages
    - Adds a VS Code task for packaging Vale style packages
    - Adds a GitHub Action for packing Vale style packages
    - Resolves #42

--- .build.ps1
+++ .build.ps1
@@ -27,3 +27,22 @@ task SyncVale {
     Copy-Item -Path $_ -Destination $SyncedStyleFolder -Force -Container -Recurse
   }
 }
+
+task PackageVale {
+  $SourceStyleFolder = Get-Item -Path $PSScriptRoot/Source/Styles
+  $PackagedStyleFolder = Join-Path $PSScriptRoot -ChildPath 'PackagedStyles'
+
+  if (Test-Path -Path $PackagedStyleFolder) {
+    Remove-Item -Path $PackagedStyleFolder -Recurse -Force
+  }
+  $null = New-Item -Path $PackagedStyleFolder -ItemType Directory
+
+  Get-ChildItem -Path $SourceStyleFolder -Directory | ForEach-Object -Process {
+    $CompressionParameters = @{
+      Path            = $_.FullName
+      DestinationPath = Join-Path -Path $PackagedStyleFolder -ChildPath "$($_.BaseName).zip"
+      Force           = $true
+    }
+    Compress-Archive @CompressionParameters
+  }
+}
