480b7eab4ae9a9b8fbca2b1bad9a26cb354a9392
Sean Wheeler <sean.wheeler@microsoft.com>
2023-02-17 06:32:05 -0600

    Refactor for class and enum

--- Source/Modules/Documentarian.ModuleAuthor/Source/Public/Functions/Get-ParameterInfo.ps1
+++ Source/Modules/Documentarian.ModuleAuthor/Source/Public/Functions/Get-ParameterInfo.ps1
@@ -1,19 +1,33 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 
-function Get-ParameterInfo {
-    [CmdletBinding()]
-    param(
-        [Parameter(Mandatory, Position = 0)]
-        [string[]]$ParameterName,
+[Flags()] enum ProviderFlags {
+    Registry    = 0x01
+    Alias       = 0x02
+    Environment = 0x04
+    FileSystem  = 0x08
+    Function    = 0x10
+    Variable    = 0x20
+    Certificate = 0x40
+    WSMan       = 0x80
+}
 
-        [Parameter(Mandatory, Position = 1)]
-        [string]$CmdletName,
+class ParameterInfo {
+    [string]$Name
+    [string]$HelpText
+    [string]$Type
+    [string]$ParameterSet
+    [string]$Aliases
+    [bool]$Required
+    [string]$Position
+    [string]$Pipeline
+    [bool]$Wildcard
+    [bool]$Dynamic
+    [bool]$FromRemaining
+    [bool]$DontShow
+    [ProviderFlags]$ProviderFlags
 
-        [switch]$AsObject
-    )
-
-    $mdtext = @'
+    hidden static [string]$MarkdownTemplate = @'
 ### -{0}
 
 {1}
@@ -26,83 +40,133 @@ Aliases: {4}
 Required: {5}
 Position: {6}
 Default value: None
-Accept pipeline input: {8}
-Accept wildcard characters: {10}
+Accept pipeline input: {7}
+Accept wildcard characters: {8}
 ```
 
 '@
 
-<#
-Had to remove these two lines from the template because they are not supported by PlatyPS.
+    <#
+    DontShow: {9}
+    Providers: {10}
+    #>
 
-Value From Remaining: {7}
-Dynamic: {9}
-#>
+    ParameterInfo(
+        [string]$Name, [string]$HelpText, [string]$Type, [string]$ParameterSet, [string]$Aliases,
+        [bool]$Required, [string]$Position, [string]$Pipeline, [bool]$Wildcard, [bool]$Dynamic,
+        [bool]$FromRemaining, [bool]$DontShow, [ProviderFlags]$ProviderFlags
+    ) {
+        $this.Name          = $Name
+        $this.HelpText      = if ($null -eq $HelpText) {
+                                  '{{Placeholder}}'
+                              } else {
+                                  $HelpText
+                              }
+        $this.Type          = $Type
+        $this.ParameterSet  = if ($ParameterSet -eq '__AllParameterSets') {
+                                  '(All)'
+                              } else {
+                                  $ParameterSet -join ', '
+                              }
+        $this.Aliases       = $Aliases -join ', '
+        $this.Required      = $Required
+        $this.Position      = if ($Position -lt 0) {
+                                  'Named'
+                              } else {
+                                  $Position
+                              }
+        $this.Pipeline        = $Pipeline
+        $this.Wildcard        = $Wildcard
+        $this.Dynamic         = $Dynamic
+        $this.FromRemaining   = $FromRemaining
+        $this.DontShow        = $DontShow
+        $this.ProviderFlags   = $ProviderFlags
+    }
 
+    [string]ToMarkdown() {
+        $newtext = [ParameterInfo]::MarkdownTemplate
+        $newtext = $newtext.replace('{0}', $this.Name)
+        $newtext = $newtext.replace('{1}', $this.HelpText)
+        $newtext = $newtext.replace('{2}', $this.Type)
+        $newtext = $newtext.replace('{3}', $this.ParameterSet)
+        $newtext = $newtext.replace('{4}', $this.Aliases)
+        $newtext = $newtext.replace('{5}', $this.Required)
+        $newtext = $newtext.replace('{6}', $this.Position)
+        $newtext = $newtext.replace('{7}', $this.Pipeline)
+        $newtext = $newtext.replace('{8}', $this.Wildcard)
+        $newtext = $newtext.replace('{9}', $this.DontShow)
+        <#
+        $ProviderName = if ($this.ProviderFlags -eq 0xFF) {
+                            'All'
+                        } else {
+                            $this.ProviderFlags.ToString()
+                        }
+        $newtext = $newtext.replace('{10}', $ProviderName)
+        #>
+        return $newtext
+    }
+}
+
+function Get-ParameterInfo {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory, Position = 0)]
+        [string[]]$ParameterName,
+
+        [Parameter(Mandatory, Position = 1)]
+        [string]$CmdletName,
+
+        [switch]$AsObject
+    )
+
+    $cmdlet = Get-Command -Name $CmdletName -ErrorAction Stop
     $providerList = Get-PSProvider
-    ## TODO: Add support for dynamic parameters that are in multiple providers.
 
     foreach ($pname in $ParameterName) {
         try {
+            $paraminfo = $null
+            $param = $null
             foreach ($provider in $providerList) {
                 Push-Location $($provider.Drives[0].Name + ':')
-                $cmdlet = Get-Command -Name $CmdletName -ErrorAction Stop
                 $param = $cmdlet.Parameters.Values | Where-Object Name -EQ $pname
                 if ($param) {
-                    $paraminfo = [PSCustomObject]@{
-                        Name          = $param.Name
-                        HelpText      = if ($null -eq $param.Attributes.HelpMessage) {
-                                            '{{Placeholder}}'
-                                        } else {
-                                            $param.Attributes.HelpMessage
-                                        }
-                        Type          = $param.ParameterType.FullName
-                        ParameterSet  = if ($param.Attributes.ParameterSetName -eq '__AllParameterSets') {
-                                            '(All)'
-                                        } else {
-                                            $param.Attributes.ParameterSetName -join ', '
-                                        }
-                        Aliases       = $param.Aliases -join ', '
-                        Required      = $param.Attributes.Mandatory
-                        Position      = if ($param.Attributes.Position -lt 0) {
-                                            'Named'
-                                        } else {
-                                            $param.Position
-                                        }
-                        FromRemaining = $param.Attributes.ValueFromRemainingArguments
-                        Pipeline      = 'ByValue ({0}), ByName ({1})' -f $param.Attributes.ValueFromPipeline,
-                                            $param.Attributes.ValueFromPipelineByPropertyName
-                        Dynamic       = if ($param.IsDynamic) {
-                                            'True ({0} provider)' -f $provider.Name
-                                        } else {
-                                            'False'
-                                        }
-                        Wildcard      = $param.Attributes.TypeId.Name -contains 'SupportsWildcardsAttribute'
+                    if ($paraminfo) {
+                        $paraminfo.ProviderFlags = $paraminfo.ProviderFlags -bor [ProviderFlags]($provider.Name)
+                    } else {
+                        $paraminfo = [ParameterInfo]::new(
+                            $param.Name,
+                            $param.Attributes.HelpMessage,
+                            $param.ParameterType.FullName,
+                            $param.Attributes.ParameterSetName,
+                            $param.Aliases,
+                            $param.Attributes.Mandatory,
+                            $param.Attributes.Position,
+                            ('ByValue ({0}), ByName ({1})' -f $param.Attributes.ValueFromPipeline, $param.Attributes.ValueFromPipelineByPropertyName),
+                            ($param.Attributes.TypeId.Name -contains 'SupportsWildcardsAttribute'),
+                            $param.IsDynamic,
+                            $param.Attributes.ValueFromRemainingArguments,
+                            $param.Attributes.DontShow,
+                            [ProviderFlags]($provider.Name)
+                        )
                     }
-                    Pop-Location
-                    break
-                } else {
-                    Pop-Location
                 }
+                Pop-Location
             }
         } catch {
             Write-Error "Cmdlet $CmdletName not found."
             return
         }
 
-        if ($param) {
+        if ($paraminfo) {
             if ($AsObject) {
                 $paraminfo
             } else {
-                $newtext = $mdtext
-                [array]$props = $paraminfo.psobject.Properties
-                for ($y = 0; $y -lt $props.Count; $y++) {
-                    $newtext = $newtext.replace("{$y}", $props[$y].Value)
-                }
-                $newtext
+                $paraminfo.ToMarkdown()
             }
         } else {
             Write-Error "Parameter $pname not found."
         }
     }
 }
+
+Get-ParameterInfo path, Options set-item -AsObject
