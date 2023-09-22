da8044199e2cf9af66b2aa51d1364051d71073bf
Santiago Squarzon <squarzon.santiago@gmail.com>
2023-09-22 14:48:37 -0300

    Adding CommandInfoArgumentTransformation class. This type is used to transform arguments to a CommandInfo instance. In addition, if the source type is already a CommandInfo instance and it is an "Alias" CommandType, it returns the ResolvedCommand instead.

---
+++ Projects/Modules/Documentarian.MicrosoftDocs/Source/Public/Classes/CommandInfoArgumentTransformation.ps1
@@ -0,0 +1,26 @@
+﻿# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+using namespace System.Management.Automation
+
+class CommandInfoArgumentTransformation : ArgumentTransformationAttribute {
+    [Object] Transform([EngineIntrinsics] $engineIntrinsics, [Object] $inputData) {
+        $cmd = $inputData
+
+        if ($inputData -isnot [CommandInfo]) {
+            $cmd = $engineIntrinsics.InvokeCommand.GetCommand(
+                [string] $inputData,
+                [CommandTypes]::All)
+        }
+
+        if (-not $cmd) {
+            throw [CommandNotFoundException]::new("Unable to find command '$inputData'.")
+        }
+
+        if ($cmd.CommandType -eq [CommandTypes]::Alias) {
+            return $cmd.ResolvedCommand
+        }
+
+        return $cmd
+    }
+}
