6f6d6184a0ccce65f5d20541643dd87a296fac88
Sean Wheeler <sean.wheeler@microsoft.com>
2024-04-21 16:31:18 -0500

    Prevent changes to code blocks

--- Projects/Modules/Documentarian/Source/Private/Functions/GetMDLinks.ps1
+++ Projects/Modules/Documentarian/Source/Private/Functions/GetMDLinks.ps1
@@ -1,11 +1,28 @@
 ﻿# Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 
+#region    RequiredFunctions
+
+$SourceFolder = $PSScriptRoot
+while ('Source' -ne (Split-Path -Leaf $SourceFolder)) {
+    $SourceFolder = Split-Path -Parent -Path $SourceFolder
+}
+$RequiredFunctions = @(
+    Resolve-Path -Path "$SourceFolder/Private/Functions/IsInCodeBlock.ps1"
+)
+foreach ($RequiredFunction in $RequiredFunctions) {
+    . $RequiredFunction
+}
+
+#endregion RequiredFunctions
+
 function GetMDLinks {
     param(
         $mdlinks, # results from regex match
         $reflinks  # results from regex match
     )
+
+    $mdlinks = $mdlinks | Where-Object { -not (IsInCodeBlock $_.LineNumber $codefences) }
     foreach ($mdlink in $mdlinks.Matches) {
         # Skip INCLUDE and tab links
         if (-not $mdlink.Value.Trim().StartsWith('[!INCLUDE') -and
@@ -26,20 +43,19 @@ function GetMDLinks {
         }
     }
 
+    $reflinks = $reflinks | Where-Object { -not (IsInCodeBlock $_.LineNumber $codefences) }
     foreach ($reflink in $reflinks.Matches) {
-        if (-not $reflink.Value.Trim().StartsWith('[!INCLUDE')) {
-            $linkitem = [pscustomobject]([ordered]@{
-                    mdlink = ''
-                    target = ''
-                    ref    = ''
-                    label  = ''
-                })
-            switch ($reflink.Groups) {
-                { $_.Name -eq 'link' } { $linkitem.mdlink = $_.Value }
-                { $_.Name -eq 'label' } { $linkitem.label = $_.Value }
-                { $_.Name -eq 'ref' } { $linkitem.ref = $_.Value }
-            }
-            $linkitem
+        $linkitem = [pscustomobject]([ordered]@{
+                mdlink = ''
+                target = ''
+                ref    = ''
+                label  = ''
+            })
+        switch ($reflink.Groups) {
+            { $_.Name -eq 'link' } { $linkitem.mdlink = $_.Value }
+            { $_.Name -eq 'label' } { $linkitem.label = $_.Value }
+            { $_.Name -eq 'ref' } { $linkitem.ref = $_.Value }
         }
+        $linkitem
     }
 }
---
+++ Projects/Modules/Documentarian/Source/Private/Functions/IsInCodeBlock.ps1
@@ -0,0 +1,16 @@
+﻿# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+function IsInCodeBlock {
+    param(
+        $linenumber,
+        $codefences
+    )
+    foreach ($fence in $codefences) {
+        if ($linenumber -ge $fence.Start -and $linenumber -le $fence.End) {
+            return $true
+        }
+    }
+    return $false
+
+}
